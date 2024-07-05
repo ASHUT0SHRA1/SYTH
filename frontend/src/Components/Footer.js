86dc38d2290d5b622688f67a10dd62338491b472
Ashutosh Rai <89987124+ASHUT0SHRA1@users.noreply.github.com>
2024-07-05 11:35:41 +0530

    edit footer in the repository

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer bg-dark text-light py-3">
       <div className="container text-center">
-        <p className="mb-0">Todo list &copy; 2024 AshutoshRai</p>
+        <p className="mb-0">&copy; 2024 AshutoshRai</p>
         <p className="mb-0">Created on 27/05/24. All rights reserved.</p>
       </div>
     </footer>
